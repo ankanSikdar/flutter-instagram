f639c23758ace202d83aa769265834f4e0eb0374
Ankan Sikdar <ankansikdar@gmail.com>
2021-05-15 17:56:13 +0530

    Added Auth State bloc

---
+++ lib/blocs/auth/auth_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:meta/meta.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super();
+
+  @override
+  Stream<AuthState> mapEventToState(
+    AuthEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
---
+++ lib/blocs/auth/auth_state.dart
@@ -0,0 +1,28 @@
+part of 'auth_bloc.dart';
+
+enum AuthStatus { unknown, authenticated, unauthenticated }
+
+class AuthState extends Equatable {
+  final auth.User user;
+  final AuthStatus status;
+
+  AuthState({
+    this.user,
+    this.status,
+  });
+
+  factory AuthState.unkown() => AuthState();
+
+  factory AuthState.authenticated({@required auth.User user}) {
+    return AuthState(user: user, status: AuthStatus.authenticated);
+  }
+
+  factory AuthState.unauthenticated() =>
+      AuthState(status: AuthStatus.unauthenticated);
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object> get props => [user, status];
+}
