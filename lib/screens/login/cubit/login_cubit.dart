7c1332e9aa5ebac3648d12cce6663406f982859f
Ankan Sikdar <ankansikdar@gmail.com>
2021-05-29 15:19:45 +0530

    login cubit designed

--- lib/screens/login/cubit/login_cubit.dart
+++ lib/screens/login/cubit/login_cubit.dart
@@ -1,10 +1,36 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:instagram_clone/models/models.dart';
+import 'package:instagram_clone/repositories/repositories.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginState.initial());
+  final AuthRepository _authRepository;
+
+  LoginCubit({@required AuthRepository authRepository})
+      : _authRepository = authRepository,
+        super(LoginState.initial());
+
+  void emailChanged(String value) {
+    emit(state.copyWith(email: value, status: LoginStatus.initial));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(password: value, status: LoginStatus.initial));
+  }
+
+  void loginWithCredentials() async {
+    if (!state.isFormValid || state.status == LoginStatus.submitting) return;
+    emit(state.copyWith(status: LoginStatus.submitting));
+    try {
+      await _authRepository.loginWithEmailAndPassword(
+          email: state.email, password: state.password);
+      // If no error
+      emit(state.copyWith(status: LoginStatus.success));
+    } on Failure catch (err) {
+      emit(state.copyWith(failure: err, status: LoginStatus.error));
+    }
+  }
 }
