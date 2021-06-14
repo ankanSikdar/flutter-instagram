56573e2e7abcf190dabfc125ecda7ddd2e6271d9
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-14 20:01:15 +0530

    Added empty screens

---
+++ lib/screens/feed/feed_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  static const String routeName = '/feed';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Feed Screen'),
+      ),
+    );
+  }
+}
