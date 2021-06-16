77865622cbe721b5d4a813c498cb744ecf483197
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-16 20:32:54 +0530

    Created Edit Profile Cubit

---
+++ lib/screens/edit_profile/cubit/edit_profile_cubit.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import 'package:instagram_clone/models/models.dart';
+import 'package:instagram_clone/repositories/repositories.dart';
+import 'package:instagram_clone/screens/profile/bloc/profile_bloc.dart';
+
+part 'edit_profile_state.dart';
+
+class EditProfileCubit extends Cubit<EditProfileState> {
+  final UserRepository _userRepository;
+  final StorageRepository _storageRepository;
+  final ProfileBloc _profileBloc;
+
+  EditProfileCubit({
+    @required UserRepository userRepository,
+    @required StorageRepository storageRepository,
+    @required ProfileBloc profileBloc,
+  })  : this._userRepository = userRepository,
+        this._storageRepository = storageRepository,
+        this._profileBloc = profileBloc,
+        super(EditProfileState.initial()) {
+    final user = _profileBloc.state.user;
+    emit(state.copyWith(username: user.username, bio: user.bio));
+  }
+
+  void profileImageChanged(File image) {
+    emit(
+      state.copyWith(profileImage: image, status: EditProfileStatus.initial),
+    );
+  }
+
+  void usernameChanged(String username) {
+    emit(
+      state.copyWith(username: username, status: EditProfileStatus.initial),
+    );
+  }
+
+  void bioChanged(String bio) {
+    emit(
+      state.copyWith(bio: bio, status: EditProfileStatus.initial),
+    );
+  }
+
+  void submit() async {
+    emit(state.copyWith(status: EditProfileStatus.submitting));
+    try {
+      final user = _profileBloc.state.user;
+
+      var profileImageUrl = user.profileImageUrl;
+      if (profileImageUrl != null) {
+        profileImageUrl = await _storageRepository.uploadProfileImage(
+          image: state.profileImage,
+          url: profileImageUrl,
+        );
+      }
+
+      final updatedUser = user.copyWith(
+        username: state.username,
+        bio: state.bio,
+        profileImageUrl: profileImageUrl,
+      );
+
+      await _userRepository.updateUser(user: updatedUser);
+
+      _profileBloc.add(ProfileLoadUser(userId: user.id));
+
+      emit(state.copyWith(status: EditProfileStatus.success));
+    } catch (error) {
+      emit(state.copyWith(
+          status: EditProfileStatus.error,
+          failure: Failure(message: 'We were unable to update your profile')));
+    }
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -7,3 +7,4 @@ export 'create_post/create_post_screen.dart';
 export 'notifications/notifications_screen.dart';
 export 'profile/profile_screen.dart';
 export 'search/search_screen.dart';
+export 'edit_profile/edit_profile_screen.dart';
