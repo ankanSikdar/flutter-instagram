10fc8f58d59b0d813b36926a5125001a856a1dd0
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-16 16:15:22 +0530

    Created UserProfileImage widget

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/repositories/repositories.dart';
+import 'package:instagram_clone/screens/profile/bloc/profile_bloc.dart';
+import 'package:instagram_clone/widgets/user_profile_image.dart';
 
 class ProfileScreen extends StatelessWidget {
   static const String routeName = '/profile';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Profile Screen'),
-      ),
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(state.user.username),
+            actions: [
+              IconButton(
+                  icon: Icon(Icons.logout),
+                  onPressed: () {
+                    context.read<AuthRepository>().logOut();
+                  })
+            ],
+          ),
+          body: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(24.0, 32.0, 24.0, 0.0),
+                  child: Row(
+                    children: [
+                      UserProfileImage(
+                        radius: 40.0,
+                        profileImageUrl: state.user.profileImageUrl,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
