ccc8779cf8105dba9a9da8012727b090bc0d2acc
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-16 17:12:37 +0530

    Added profile info widget

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instagram_clone/repositories/repositories.dart';
 import 'package:instagram_clone/screens/profile/bloc/profile_bloc.dart';
+import 'package:instagram_clone/screens/profile/widgets/profile_info.dart';
 import 'package:instagram_clone/screens/profile/widgets/profile_stats.dart';
 import 'package:instagram_clone/widgets/user_profile_image.dart';
 
@@ -27,23 +28,37 @@ class ProfileScreen extends StatelessWidget {
           body: CustomScrollView(
             slivers: [
               SliverToBoxAdapter(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(24.0, 32.0, 24.0, 0.0),
-                  child: Row(
-                    children: [
-                      UserProfileImage(
-                        radius: 40.0,
-                        profileImageUrl: state.user.profileImageUrl,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(24.0, 32.0, 24.0, 0.0),
+                      child: Row(
+                        children: [
+                          UserProfileImage(
+                            radius: 40.0,
+                            profileImageUrl: state.user.profileImageUrl,
+                          ),
+                          ProfileStats(
+                            isCurrentUser: state.isCurrentUser,
+                            isFollowing: state.isFollowing,
+                            posts: 0, // state.posts.length
+                            followers: state.user.followers,
+                            following: state.user.following,
+                          ),
+                        ],
                       ),
-                      ProfileStats(
-                        isCurrentUser: state.isCurrentUser,
-                        isFollowing: state.isFollowing,
-                        posts: 0, // state.posts.length
-                        followers: state.user.followers,
-                        following: state.user.following,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 10.0,
                       ),
-                    ],
-                  ),
+                      child: ProfileInfo(
+                        username: state.user.username,
+                        bio: state.user.bio,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
---
+++ lib/screens/profile/widgets/profile_info.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ProfileInfo extends StatelessWidget {
+  final String username;
+  final String bio;
+
+  const ProfileInfo({
+    Key key,
+    @required this.username,
+    @required this.bio,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          username,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        Text(
+          bio,
+          style: TextStyle(
+            fontSize: 15.0,
+          ),
+        ),
+        Divider(),
+      ],
+    );
+  }
+}
