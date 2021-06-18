7a0ca77adf7cb2a868f56c7f8d10bef5b5ce1250
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-18 20:50:01 +0530

    firebase functions follow and unfollow

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,91 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.onFollowUser = functions.firestore
+  .document("/followers/{userId}/userFollowers/{followerId}")
+  .onCreate(async (_, context) => {
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    // Increase followed user's follower count
+    const followedUserRef = admin.firestore().collection("users").doc(userId);
+    const followedUserDoc = await followedUserRef.get();
+    if (followedUserDoc.get("followers") !== undefined) {
+      followedUserRef.update({
+        followers: followedUserDoc.get("followers") + 1,
+      });
+    } else {
+      followedUserRef.update({ followers: 1 });
+    }
+
+    // Increase user's following count
+    const userRef = admin.firestore().collection("users").doc(followerId);
+    const userDoc = await userRef.get();
+    if (userDoc.get("following") !== undefined) {
+      userRef.update({ following: userDoc.get("following") + 1 });
+    } else {
+      userRef.update({ following: 1 });
+    }
+
+    // Add followed user's post to user's post feed
+    const followedUserPostRef = admin
+      .firestore()
+      .collection("posts")
+      .where("author", "==", followedUserRef);
+
+    const userFeedRef = admin
+      .firestore()
+      .collection("feeds")
+      .doc(followerId)
+      .collection("userFeed");
+
+    const followedUserPostsSnapshot = await followedUserPostRef.get();
+    followedUserPostsSnapshot.forEach((doc) => {
+      if (doc.exists) {
+        userFeedRef.doc(doc.id).set(doc.data());
+      }
+    });
+  });
+
+exports.onUnfollowUser = functions.firestore
+  .document("/followers/{userId}/userFollowers/{followerId}")
+  .onDelete(async (_, context) => {
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    // Decrease unfollowed user's follower count
+    const followedUserRef = admin.firestore().collection("users").doc(userId);
+    const followedUserDoc = await followedUserRef.get();
+    if (followedUserDoc.get("followers") !== undefined) {
+      followedUserRef.update({
+        followers: followedUserDoc.get("followers") - 1,
+      });
+    } else {
+      followedUserRef.update({ followers: 0 });
+    }
+
+    // Decrement user's following count
+    const userRef = admin.firestore().collection("users").doc(followerId);
+    const userDoc = await userRef.get();
+    if (userDoc.get("following") !== undefined) {
+      userRef.update({ following: userDoc.get("following") - 1 });
+    } else {
+      userRef.update({ following: 0 });
+    }
+
+    // Remove unfollowed user's posts from user's post feed
+    const userFeedRef = admin
+      .firestore()
+      .collection("feeds")
+      .doc(followerId)
+      .collection("userFeed")
+      .where("author", "==", followedUserRef);
+    const userPostsSnapshot = await userFeedRef.get();
+    userPostsSnapshot.forEach((doc) => {
+      if (doc.exists) {
+        doc.ref.delete();
+      }
+    });
+  });
