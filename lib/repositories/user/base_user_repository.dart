fce7608bac5ac2906593a3ac5699bb326dd9421d
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-18 13:12:32 +0530

    Updated user repository with search functionality

--- lib/repositories/user/base_user_repository.dart
+++ lib/repositories/user/base_user_repository.dart
@@ -4,4 +4,5 @@ import 'package:meta/meta.dart';
 abstract class BaseUserRepository {
   Future<User> getUserWithId({@required String userId});
   Future<void> updateUser({@required User user});
+  Future<List<User>> searchUsers({@required String query});
 }
